e8f66e760d48fbd58e53d2a5121f30bc3d9f3479
Adarsh <adarshyeligala09@gmail.com>
2022-10-15 18:43:34 +0530

    One way and Two Way Data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'practice';
+  title = "practice";
+  firstName = "Adarsh Yeligala"
+  lastName = "Roger Federer";
+  fullName = "Adarsh Kumar Yeligala";
 }
